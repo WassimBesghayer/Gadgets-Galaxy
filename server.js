dace6fb56ea07da5bb4dd15ccf87747d961d84de
Wassim <wassim.besghayer@gmail.com>
2025-01-26 22:59:29 +0100

    cors library

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express=require("express");
 const app=express();
-
+const cors=require("cors");
 const db_connect=require("./connect")
 require("dotenv").config();
 db_connect()
 
 app.use(express.json())
+app.use(cors())
 app.use("/product", require("./routes/product"))
+app.use("/commande", require("./routes/commande"))
 
 app.listen(process.env.port, (err)=>err?console.log(err):console.log("server is runnig well !"))
