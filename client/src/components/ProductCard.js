8995f6c58aecde3d0b50b2daee8e1a5285275c13
Wassim <wassim.besghayer@gmail.com>
2025-01-28 03:00:42 +0100

    1st commit

---
+++ client/src/components/ProductCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
+function ProductCard(product) {
+  return (
+    <div>
+         <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={product?.img} />
+      <Card.Body>
+        <Card.Title>{product?.title}</Card.Title>
+        <Card.Text>
+            {product?.description}
+            {product?.price}
+        </Card.Text>
+        <Button variant="primary">Buy it</Button>
+      </Card.Body>
+    </Card>
+    </div>
+  )
+}
+
+export default ProductCard
