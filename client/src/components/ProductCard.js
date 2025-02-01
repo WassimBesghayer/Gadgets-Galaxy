ed89449063f5b2368a9b9478ded8a12d37cec93a
Wassim <wassim.besghayer@gmail.com>
2025-02-01 16:58:12 +0100

    enable pinging

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { deleteproduct } from '../redux/productslice';
+import { useDispatch } from 'react-redux';
+import EditProduct from './EditProduct';
+import { addorder } from '../redux/orderSlice';
 
 
-function ProductCard({product}) {
+function ProductCard({product, ping, setping}) {
+  const dispatch=useDispatch();
+  const [neworder, setneworder] = useState({
+    
+    nameUser:"user",
+    nameProduct:product?.title,
+    price:product?.price,
+    date:new Date()
+  })
   return (
     <div className='productCard-div'>
       <Card style={{ width: '18rem' }}>
@@ -11,9 +23,11 @@ function ProductCard({product}) {
       <Card.Body>
         <Card.Title>{product?.title}</Card.Title>
         <Card.Text>
-          {product?.price}
+          <h3>{product?.price} TND</h3>
         </Card.Text>
-        <Button variant="primary">Buy it</Button>
+        <Button variant="primary" onClick={()=>{dispatch(addorder(neworder));setping(!ping)}}>Buy it</Button>
+        <Button variant="primary" onClick={()=>{dispatch(deleteproduct(product?._id)); setping(!ping)}}>Delete it</Button>
+        <EditProduct product={product} ping={ping} setping={setping}/>
       </Card.Body>
     </Card>
     </div>
