3a679d0b19499ac67e34a2b959a7d095f2e1f243
Wassim <wassim.besghayer@gmail.com>
2025-02-01 16:50:00 +0100

    enable pinging + redux slice

--- client/src/components/Orders.js
+++ client/src/components/Orders.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Table from 'react-bootstrap/Table';
+import { deleteorder } from '../redux/orderSlice';
+import Button from 'react-bootstrap/Button';
 
-function Orders() {
+
+function Orders({ping, setping}) {
+  const orders=useSelector((state)=>state.order.orderList);
+  const dispatch=useDispatch();
+  console.log(orders)
   return (
     <div>
-        
+      <Table style={{fontSize:"14px", width:"50%"}}>
+      <thead >
+        <tr>
+          <th>#</th>
+          <th>User Full Name</th>
+          <th>Product bought</th>
+          <th>Product Price</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody  >
+      {orders?.map((el)=><tr><td></td>
+                              <td>{el.nameUser}</td>
+                              <td>{el.nameProduct}</td>
+                              <td>{el.price}</td>
+                              <td>{el.date}</td>
+                              <td>edit</td>
+                              <td> <Button variant="primary" onClick={()=>{dispatch(deleteorder(el?._id)); setping(!ping)}}>x</Button></td>
+                              </tr>)}
+      
+      </tbody>
+    </Table>
+    
     </div>
   )
 }
