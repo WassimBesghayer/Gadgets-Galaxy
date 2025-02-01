6efa74f8c018eb4275ec04a736266d5e70c73215
Wassim <wassim.besghayer@gmail.com>
2025-02-01 16:54:53 +0100

    modal+redux

--- client/src/components/AddProduct.js
+++ client/src/components/AddProduct.js
@@ -4,115 +4,106 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { useDispatch } from 'react-redux';
+import { addproduct } from '../redux/productslice';
 
 
-function AddProduct() {
+
+function AddProduct({ping, setping}) {
+  const dispatch=useDispatch()
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const [newproduct, setnewproduct] = useState({
+    "title": "",
+    "img":"",
+    "description":"",
+    "price":'',
+    "category": ""
+  })
   return (
     <div>
-        <Form noValidate onSubmit={handleSubmit}>
-          <Row className="mb-3">
-            <Form.Group as={Col} md="4" controlId="validationFormik01">
-              <Form.Label>First name</Form.Label>
-              <Form.Control
-                type="text"
-                name="firstName"
-                value={values.firstName}
-                onChange={handleChange}
-                isValid={touched.firstName && !errors.firstName}
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationFormik02">
-              <Form.Label>Last name</Form.Label>
-              <Form.Control
-                type="text"
-                name="lastName"
-                value={values.lastName}
-                onChange={handleChange}
-                isValid={touched.lastName && !errors.lastName}
-              />
 
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationFormikUsername">
-              <Form.Label>Username</Form.Label>
-              <InputGroup hasValidation>
-                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                <Form.Control
-                  type="text"
-                  placeholder="Username"
-                  aria-describedby="inputGroupPrepend"
-                  name="username"
-                  value={values.username}
-                  onChange={handleChange}
-                  isInvalid={!!errors.username}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors.username}
-                </Form.Control.Feedback>
-              </InputGroup>
-            </Form.Group>
-          </Row>
-          <Row className="mb-3">
-            <Form.Group as={Col} md="6" controlId="validationFormik03">
-              <Form.Label>City</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="City"
-                name="city"
-                value={values.city}
-                onChange={handleChange}
-                isInvalid={!!errors.city}
-              />
+<>
+      <Button className='click-button' id='add-btn' variant="primary" onClick={handleShow} style={{backgroundColor:"green", border: 'none'}}>
+        Add Item
+      </Button>
 
-              <Form.Control.Feedback type="invalid">
-                {errors.city}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="3" controlId="validationFormik04">
-              <Form.Label>State</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="State"
-                name="state"
-                value={values.state}
-                onChange={handleChange}
-                isInvalid={!!errors.state}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.state}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="3" controlId="validationFormik05">
-              <Form.Label>Zip</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Zip"
-                name="zip"
-                value={values.zip}
-                onChange={handleChange}
-                isInvalid={!!errors.zip}
-              />
 
-              <Form.Control.Feedback type="invalid">
-                {errors.zip}
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Row>
-          <Form.Group className="mb-3">
-            <Form.Check
-              required
-              name="terms"
-              label="Agree to terms and conditions"
-              onChange={handleChange}
-              isInvalid={!!errors.terms}
-              feedback={errors.terms}
-              feedbackType="invalid"
-              id="validationFormik0"
-            />
-          </Form.Group>
-          <Button type="submit">Submit form</Button>
-        </Form>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>New Item to be added :</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Title</Form.Label>
+        <Form.Control type="text" placeholder="Enter text" onChange={(e)=>setnewproduct({...newproduct, title:e.target.value})}/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Image</Form.Label>
+        <Form.Control type="text" placeholder="Insert image URL" onChange={(e)=>setnewproduct({...newproduct, image:e.target.value})}/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Description</Form.Label>
+        <Form.Control type="text" placeholder="Enter text" onChange={(e)=>setnewproduct({...newproduct, description:e.target.value})}/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Price</Form.Label>
+        <Form.Control type="text" placeholder="Enter number" onChange={(e)=>setnewproduct({...newproduct, price:e.target.value})}/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Category</Form.Label>
+        <Form.Select aria-label="Default select example" placeholder="Open this select menu" onChange={(e)=>setnewproduct({...newproduct, category:e.target.value})}>
+          <option value="Smart Phones">Smart Phones</option>
+          <option value="Smart Watches">Smart Watches</option>
+          <option value="Embedded Systems">Embedded Systems</option>
+          <option value="Remote Controllers">Remote Controllers</option>
+        </Form.Select>    
+      </Form.Group>
+
+      </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose} style={{backgroundColor:"red"}}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={()=>{dispatch(addproduct(newproduct));setping(!ping);handleClose()}}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+
+    {/* <>
+        <Button className='click-button' id='rm-btn' variant="primary" onClick={handleShow} style={{backgroundColor:"red", border: 'none'}}>
+          Remove Item
+        </Button>
+
+        <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>New Item to be added :</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>ID</Form.Label>
+        <Form.Control type="text" placeholder="Enter the ID code" />
+      </Form.Group>
+      </Form>
+      </Modal.Body>
+    </>   */}
+      
+   
+      
     </div>
   )
 }
